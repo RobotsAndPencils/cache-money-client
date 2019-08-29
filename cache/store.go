82beb8a6af04e176cf16da611db0cf4f31f3551d
Nathan Youngman <4566+nathany@users.noreply.github.com>
2019-08-29 15:44:03 -0600

    compressing a folder (recursively)
    ```
    CACHE_KEY="v1-{{ checksum \"go.mod\" }}" CACHE_PATH=./cache/fixtures/node_modules/ TOKEN=123 ENDPOINT=http://localhost:4000/api go run main.go store
    ```

--- cache/store.go
+++ cache/store.go
@@ -1,6 +1,19 @@
 package cache
 
-import "os"
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const mimeType = "application/zip"
+
+var logger = log.New(os.Stderr, "", 0)
 
 // Store a path to the cache
 func Store(client *Client, key, path string) error {
@@ -14,11 +27,61 @@ func Store(client *Client, key, path string) error {
 		return nil
 	}
 
-	// TODO: compress path
-	f, err := os.Open(path)
+	tmpfile, err := ioutil.TempFile("", fmt.Sprintf("cache-%v-*.zip", key))
 	if err != nil {
 		return err
 	}
-	defer f.Close()
-	return client.Push(key, "application/octet-stream", f)
+	logger.Printf("Compressing %v", path)
+	defer func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}()
+
+	// compress path
+	err = compress(path, tmpfile)
+	if err != nil {
+		return err
+	}
+
+	_, err = tmpfile.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	logger.Printf("Pushing %v", filepath.Base(tmpfile.Name()))
+	return client.Push(key, mimeType, tmpfile)
+}
+
+func compress(root string, w io.Writer) error {
+	zw := zip.NewWriter(w)
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() && strings.HasPrefix(info.Name(), ".") {
+			return filepath.SkipDir // ignore hidden folders like .bin and .cache
+		} else if info.IsDir() {
+			return nil
+		}
+		rel, err := filepath.Rel(root, path)
+		if err != nil {
+			return err
+		}
+		logger.Printf("  adding %v", rel)
+
+		zf, err := zw.Create(rel)
+		if err != nil {
+			return err
+		}
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(zf, f)
+		return err
+	})
+	if err != nil {
+		return err
+	}
+	return zw.Close()
 }
