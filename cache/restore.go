448ecbd3aa9eeabe9fcb54ca691b4db015a7eac3
Nathan Youngman <4566+nathany@users.noreply.github.com>
2019-08-30 16:13:47 -0600

    use @mholt’s archiver to handle compression
    switch to tar.gz

--- cache/restore.go
+++ cache/restore.go
@@ -1,19 +1,16 @@
 package cache
 
 import (
-	"archive/zip"
-	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strings"
+
+	"github.com/mholt/archiver"
 )
 
 // Restore a path to the cache
 func Restore(client *Client, key, path string) error {
-	tmpfile, err := ioutil.TempFile("", fmt.Sprintf("cache-%v-*.zip", key))
+	tmpfile, err := ioutil.TempFile("", fmt.Sprintf("cache-%v-*.tar.gz", key))
 	if err != nil {
 		return err
 	}
@@ -30,58 +27,5 @@ func Restore(client *Client, key, path string) error {
 	//  decompress to path
 	logger.Printf("Extracting to %v", path)
 
-	zr, err := zip.OpenReader(tmpfile.Name())
-	if err != nil {
-		return err
-	}
-	defer zr.Close()
-
-	// lastDir := ""
-	for _, zf := range zr.File {
-		// dir := filepath.Dir(zf.Name)
-		// if dir != lastDir {
-		// 	logger.Printf("  extracting %v", dir)
-		// 	lastDir = dir
-		// }
-
-		err := extract(zf, path)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func extract(zf *zip.File, path string) error {
-	file := filepath.Join(path, zf.Name)
-	err := os.MkdirAll(filepath.Dir(file), 0755)
-	if err != nil {
-		return err
-	}
-
-	r, err := zf.Open()
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	logger.Printf("%v %t", file, isSymlink(zf.FileHeader.FileInfo()))
-
-	if isSymlink(zf.FileHeader.FileInfo()) {
-		buf := new(bytes.Buffer)
-		_, err := io.Copy(buf, r)
-		if err != nil {
-			return fmt.Errorf("extract symlink %v: %v", file, err)
-		}
-		return os.Symlink(file, strings.TrimSpace(buf.String()))
-	}
-
-	w, err := os.Create(file)
-	if err != nil {
-		return err
-	}
-	defer w.Close()
-
-	_, err = io.Copy(w, r)
-	return err
+	return archiver.Unarchive(tmpfile.Name(), path)
 }
--- cache/store.go
+++ cache/store.go
@@ -1,13 +1,12 @@
 package cache
 
 import (
-	"archive/zip"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/mholt/archiver"
 )
 
 const mimeType = "application/zip"
@@ -27,80 +26,22 @@ func Store(client *Client, key, path string) error {
 		return nil
 	}
 
-	tmpfile, err := ioutil.TempFile("", fmt.Sprintf("cache-%v-*.zip", key))
-	if err != nil {
-		return err
-	}
-	defer func() {
-		tmpfile.Close()
-		os.Remove(tmpfile.Name())
-	}()
+	tmpfile := filepath.Join(os.TempDir(), fmt.Sprintf("cache-%v.tar.gz", key))
+	defer os.Remove(tmpfile)
 
 	// compress path
 	logger.Printf("Compressing %v", path)
-	err = compress(path, tmpfile)
-	if err != nil {
-		return err
-	}
 
-	_, err = tmpfile.Seek(0, io.SeekStart)
+	err = archiver.Archive([]string{path}, tmpfile)
 	if err != nil {
 		return err
 	}
 
 	logger.Printf("Pushing %v", key)
-	return client.Push(key, mimeType, tmpfile)
-}
-
-func compress(root string, w io.Writer) error {
-	zw := zip.NewWriter(w)
-	// lastDir := ""
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			return nil
-		}
-		rel, err := filepath.Rel(root, path)
-		if err != nil {
-			return err
-		}
-
-		// dir := filepath.Dir(rel)
-		// if dir != lastDir {
-		// 	logger.Printf("  adding %v", dir)
-		// 	lastDir = dir
-		// }
-
-		zf, err := zw.Create(rel)
-		if err != nil {
-			return err
-		}
-
-		if isSymlink(info) {
-			linkTarget, err := os.Readlink(path)
-			if err != nil {
-				return fmt.Errorf("compress symlink %v: %v", path, err)
-			}
-			_, err = zf.Write([]byte(filepath.ToSlash(linkTarget)))
-			return err
-		}
-
-		f, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		_, err = io.Copy(zf, f)
-		f.Close()
-		return err
-	})
+	f, err := os.Open(tmpfile)
 	if err != nil {
 		return err
 	}
-	return zw.Close()
-}
-
-func isSymlink(fi os.FileInfo) bool {
-	return fi.Mode()&os.ModeSymlink != 0
+	defer f.Close()
+	return client.Push(key, mimeType, f)
 }
